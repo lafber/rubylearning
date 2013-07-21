e91f4910675064fa076bb0dcf73d36b5375fa4e2
lafber <lafber@gmail.com>
2013-07-21 18:31:15 +0200

    exercice 11 modifié

--- week3/week3exercice11.rb
+++ week3/week3exercice11.rb
@@ -4,5 +4,4 @@ Exercise11. Write a Ruby program that, when given an array: collection = [12, 23
 
 
 collection = [12, 23, 456, 123, 4579]
-
 collection.each{ |number| puts ("Number #{number} is " + ((number.to_i.odd?) ? 'odd' : 'even')) }
