7350e8133ef576e217c7f7b4cc496052a3426ee0
lafber <lafber@gmail.com>
2013-07-21 18:26:01 +0200

    Exercice 11 week 3

---
+++ week3/week3exercice11.rb
@@ -0,0 +1,8 @@
+=begin
+Exercise11. Write a Ruby program that, when given an array: collection = [12, 23, 456, 123, 4579] prints each number, and tells you whether it is odd or even.
+=end
+
+
+collection = [12, 23, 456, 123, 4579]
+
+collection.each{ |number| puts ("Number #{number} is " + ((number.to_i.odd?) ? 'odd' : 'even')) }
