89b8c211a32a1a04801738dd53e9b83e7effc377
lafber <lafber@gmail.com>
2013-07-21 17:53:00 +0200

    week 3 exercice 9

--- week3/week3exercice9.rb
+++ week3/week3exercice9.rb
@@ -2,15 +2,14 @@
 Exercise9. Given a string, write a program to reverse the word order (rather than character order). The String and Array classes provide methods which will be very helpful for solving this exercise.
 =end
 
-require(./prompt.rb)
+require('../prompt.rb')
 
 # Ask for the sentence
 sentence = prompt('Please enter the sentence to encode')
 
 # split the sentence at word
-word = sentence.split(' ')
+new_sentence = sentence.split(' ')
 
-# reverse each word  in a new array
-word.each { | element |   element.reverse!} 
+# reverse each word in a new array and join each element separated by a space
+puts new_sentence.reverse!.join(' ')
 
-puts sentence
