23991245ee385cac36402bc32d15876a883d2e61
lafber <lafber@gmail.com>
2013-07-24 23:56:58 +0200

    improvements

--- week3/week3exercice2.rb
+++ week3/week3exercice2.rb
@@ -6,20 +6,25 @@ LearningRuby Week 3 Exercice 2
 source_sentence    = 'word'
 target_sentence    = 'inserted word'
 
-# Open the source texte file as an Array
-source_file = File.exists?('./week3exercice2-source.txt') ? File.open('./week3exercice2-source.txt','r') : nil
+# Get file name
+file_name = ARGV[0] 
 
-if not source_file 
- puts 'source file not found'
- exit
+# Read the text file and put the content in a string
+source_content = File.read(file_name) if  File.exists?(file_name)
+
+if source_content
+  
+  source_content.gsub!(source_sentence, target_sentence)
+  
+# Open the text file in write mode
+  target_file = File.open(file_name, 'w') 
+
+# put in file the source file lines with replacement of the target sentence
+  target_file.puts(source_content)
+  target_file.close
+
+  puts 'file succesfully treated'
+
+elsif
+  puts "File #{file_name} unknown o unreadable"
 end
-
-# Create or reset the target file in read-write mode
-target_file = File.open('./week3exercice2-target.txt', 'w+') 
-
-# put in target file the array of source file lines with replacement of the target sentence
-target_file.puts(source_file.readlines.each{ |line| line.sub!(source_sentence, target_sentence) })
-target_file.close
-
-puts 'target file succesfully treated'
-
