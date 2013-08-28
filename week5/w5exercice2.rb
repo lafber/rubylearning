0323d4c362ce8fb974edce32c2917d97e23707f2
Bertrand LAFOUGERE <lafber@gmail.com>
2013-08-29 00:17:00 +0200

    remove unnecessary initialize

--- week5/w5exercice2.rb
+++ week5/w5exercice2.rb
@@ -1,73 +1,89 @@
 # encoding: UTF-8
-
+ 
+=begin
+"There will be an amoeba shape on the screen, with the others. When the user clicks on the amoeba, it will rotate like the others, and play a .hif sound file. The other figures like rectangle rotate around the center. I want the amoeba shape to rotate around a point on one end, like a clock hand !"
+=end
+ 
+ 
 # Shape Class
 class Shape 
-
+ 
   attr_reader :name
-
+ 
   @@sound_uri_folder = '/assets/sound/shape'
-
+ 
   def initialize(name)
     @name = name
   end
-
-  def click()
+ 
+  def on_click()
     rotate(360)
     play_sound()
   end
-
+ 
+ protected
+ 
   def rotate(angle)
-    puts "rotation of the #{self.class} #{@name} by #{angle}°."
+   "rotation of the #{self.class} #{@name} by #{angle}°."
   end
   
   def play_sound
-    #gemsound.play("#{@@sound_uri_folder}/#{self.class}.mp3")
-    puts "#{@name} is playing #{@@sound_uri_folder}/#{self.class}.aif "
+    "#{@name} is playing #{@@sound_uri_folder}/#{self.class}.aif "
   end
-
-  def to_s
-    "#{self.class} named #{@name}"
-  end
+ 
+  private
+ 
+    def to_s
+      "#{self.class} named #{@name}"
+    end
     
 end
-
-
+ 
+ 
 class Square < Shape
-    
-  def initialize(name)
-    super(name)
-  end
-  
+   
 end
-
-
+ 
+ 
 class Circle < Shape
   
-  def initialize(name)
-    super(name)
-  end
-  
 end
   
-
+ 
 class Triangle < Shape
+
+end
+ 
+ 
+class Amoeba < Shape
+
   
-  def initialize(name)
-    super(name)
-  end
+  protected
+  
+    def rotate(angle)
+      "rotation of the #{self.class} #{@name} by #{angle}° around point on one end."
+    end
+  
+    def play_sound
+      "#{@name} is playing #{@@sound_uri_folder}/#{self.class}.hif "
+    end
   
 end
-
-
+ 
   ###########
  #  MAIN   #
 ###########
-
-my_square   = Square.new('Square1')
-my_circle   = Circle.new('Circle1')
-my_triangle = Triangle.new('Triangle1') 
-
-my_square.click()
-my_circle.click()
-my_triangle.click()
-
+ 
+ 
+my_shapes = [
+  Square.new('MySquare'),
+  Circle.new('MyCircle'),
+  Triangle.new('MyTriangle'),
+  Amoeba.new('MyAmoeba'),
+]
+ 
+# Called when the respective shapes are clicked
+my_shapes.each do |shape|
+  puts shape
+  puts shape.on_click
+end
